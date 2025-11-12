2c0e0068be9493bc06b6bf0d685c7957f7b02ebb
alishabagwan38 <alishabagwan38@gmail.com>
2025-11-12 16:17:57 +0530

    hello

--- src/test/java/runner/HealthInsuranceRunner.java
+++ src/test/java/runner/HealthInsuranceRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions
 (
-		features = {"C:\\Users\\sai\\eclipse-workspace\\Coverfox_Demo\\src\\test\\resources\\coverFoxFeatures\\coverfox.feature"},
+		features = {"D:\\31st-evening-\\src\\test\\resources\\coverFoxFeatures\\coverfox.feature"},
 		glue = {"stepDefinitions","hooks"}, //tags = ""
 		plugin = {"pretty", "html:target/cucumber-reports/reports.html"},
 		publish = true,
